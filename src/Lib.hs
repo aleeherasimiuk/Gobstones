e02ae7f3055318f797cf77befe56bd7717ad48a9
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-06 16:49:09 -0300

    Ahora el cabezal puede moverse, si está dentro de los límites

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,18 @@
 module Gobstones where
+
 ----------
 --Punto 1-
 ----------
 
 data Bolita = Rojo | Azul | Verde | Negro deriving(Show)
-data Direccion = Norte | Sur | Este | Oeste
+data Direccion = Norte | Sur | Este | Oeste deriving(Show)
 
-type Cabezal = (Int, Int) 
+type Posicion = (Int, Int)
+type Cabezal = (Int, Int)
 type Tamaño  = (Int, Int)
 
 data Celda = Celda{
-  posicion :: (Int, Int),
+  posicion :: Posicion,
   bolitas  :: [Bolita]
 } deriving(Show)
 
@@ -20,9 +22,44 @@ data Tablero = Tablero{
   cabezal :: Cabezal
 } deriving(Show)
 
+mapCabezal :: (Cabezal -> Cabezal) -> Tablero -> Tablero
+mapCabezal f tablero = tablero{cabezal = f . cabezal $ tablero}
+
+
+----------
+--Punto 2-
+----------
+
 inicializarTablero :: Tamaño -> Tablero
 inicializarTablero (filas, columnas) = Tablero [Celda (x, y) [] | x <-[1 .. filas], y <- [1 .. columnas]] (filas, columnas) (1, 1)
 
 
+-------------
+--Punto 3.a--
+-------------
+
+mover :: Direccion -> Tablero -> Tablero
+mover direccion tablero
+  | puedeMoverse direccion tablero = moverCabezal direccion tablero
+  | otherwise = error "Boom"
+
+moverCabezal :: Direccion -> Tablero -> Tablero
+moverCabezal Norte = mapCabezal (sumaParOrdenado ( 0, 1))
+moverCabezal Sur   = mapCabezal (sumaParOrdenado ( 0,-1))
+moverCabezal Este  = mapCabezal (sumaParOrdenado ( 1, 0))
+moverCabezal Oeste = mapCabezal (sumaParOrdenado (-1, 0))
+
+puedeMoverse :: Direccion -> Tablero -> Bool
+puedeMoverse unaDireccion tablero = betweenDupla (1, 1) (tamaño tablero) . cabezal . moverCabezal unaDireccion $ tablero
+
+
+betweenDupla :: Ord a => (a, a) -> (a, a) -> (a, a) -> Bool
+betweenDupla (lix, liy) (lsx, lsy) (x, y) = between lix lsx x && between liy lsy y
+
+between :: Ord a => a -> a -> a -> Bool
+between li ls x = x >= li && x <= ls
+
+sumaParOrdenado :: (Int, Int) -> (Int, Int) -> (Int, Int)
+sumaParOrdenado (a, b) (c, d) = (a + c, b + d)
 
 
